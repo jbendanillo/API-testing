e575ac6fe69d494843469aa549b42e5fc7eaec4d
Jeniza <jeniza.bendanillo@gmail.com>
2021-08-04 23:14:17 +1200

    Adding Billling Order API and NUnit Tests

---
+++ NUnitTestProject/API/BillingOrderAPI.cs
@@ -0,0 +1,72 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitTestProject.API
+{
+    public class BillingOrderAPI
+    {
+        private readonly string baseUrl = "http://api.qaauto.co.nz/api/v1";
+
+        public IRestResponse GetOrderById(string id)
+        {
+            //setting task
+            var client = new RestClient($"{baseUrl}/BillingOrder/{id}");
+            var request = new RestRequest(Method.GET);
+            //Execute task
+            IRestResponse response = client.Execute(request);
+            return response;
+        }
+
+        public IRestResponse GetOrderAll()
+        {
+            //setting task
+            var client = new RestClient($"{baseUrl}/billingOrders");
+            var request = new RestRequest(Method.GET);
+            //Execute task
+            IRestResponse response = client.Execute(request);
+            return response;
+        }
+
+        public IRestResponse PostOrder(string body)
+        {
+            //setting task
+            var client = new RestClient($"{baseUrl}/BillingOrder");
+            var request = new RestRequest(Method.POST);
+
+            request.AddHeader("Content-Type", "application/json");
+            request.AddParameter("application/json", body, ParameterType.RequestBody);
+
+            //Execute task
+            IRestResponse response = client.Execute(request);
+            return response;
+        }
+
+        public IRestResponse UpdateOrder(string id, string body)
+        {
+            //setting task
+            var client = new RestClient($"{baseUrl}/BillingOrder/{id}");
+            var request = new RestRequest(Method.PUT);
+
+            request.AddParameter("application/json", body, ParameterType.RequestBody);
+
+            //Execute task
+            IRestResponse response = client.Execute(request);
+            return response;
+
+        }
+
+        public IRestResponse DeleteByOrder(string id)
+        {
+            //setting task
+            var client = new RestClient($"{baseUrl}/BillingOrder/{id}");
+            var request = new RestRequest(Method.DELETE);
+
+
+            //Execute task
+            IRestResponse response = client.Execute(request);
+            return response;
+        }
+    }
+}
--- NUnitTestProject/UnitTest1.cs
+++ NUnitTestProject/UnitTest1.cs
@@ -1,4 +1,6 @@
 using NUnit.Framework;
+using NUnitTestProject.API;
+using RestSharp;
 
 namespace NUnitTestProject
 {
@@ -9,5 +11,64 @@ namespace NUnitTestProject
         {
         }
 
+        BillingOrderAPI billingOrder = new BillingOrderAPI();
+
+        [Test]
+        public void TC_GetOrder()
+        {
+
+            IRestResponse response = billingOrder.GetOrderById("2");
+            TestContext.WriteLine(response.Content);
+
+            IRestResponse response1 = billingOrder.GetOrderById("1");
+            TestContext.WriteLine(response1.Content);
+        }
+
+        [Test]
+        public void TC_GetAllOrder()
+        {
+
+            IRestResponse response = billingOrder.GetOrderAll();
+            TestContext.WriteLine(response.Content);
+
+
+        }
+
+
+        [Test]
+        public void TC_PostOrder()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                string body = $"{{\"addressLine1\": \"Park\",\"addressLine2\": \"Street\", \"city\": \"Holroyd\", \"comment\": \"Newrecord\",\"email\": \"john_nel@gmail.com\",\"firstName\": \"John\",\"itemNumber\":{i},\"lastName\": \"Nelson\",\"phone\": \"0987654432\",\"state\": \"AL\",\"zipCode\": \"879652\"}}";
+
+                //for (int i = 1; i <= 100; i++)
+                //{
+                //    string body = $"{{\"firstName\":\"Csharp{i}\",\"itemNumber\":{i},\"addressLine1\":\"test\",\"addressLine2\": \"ddd\",\"city\": \"CA\",\"comment\": \"testing\",\"email\": \"aa@aa.com\", \"lastName\": \"dddd\",\"phone\": \"1111111111\", \"state\": \"AK\",\"zipCode\": \"222222\"}}";
+
+                // string body = "{\"addressLine1\": \"Home\",\"addressLine2\": \"Street\", \"city\": \"Holroyd\", \"comment\": \"Newrecord\",\"email\": \"john_nel@gmail.com\",\"firstName\": \"John\",\"id\": \"0\",\"itemNumber\": \"3\",\"lastName\": \"Nelson\",\"phone\": \"0987654432\",\"state\": \"AL\",\"zipCode\": \"879652\"}";
+                IRestResponse response = billingOrder.PostOrder(body);
+                TestContext.WriteLine(response.Content);
+            }
+
+        }
+
+        [Test]
+        public void TC_UpdateOrder()
+        {
+            string body = "{\"addressLine1\": \"Strathfield\",\"addressLine2\": \"Street\", \"city\": \"Holroyd\", \"comment\": \"Newrecord\",\"email\": \"john_nel@gmail.com\",\"firstName\": \"John1\",\"id\": \"0\",\"itemNumber\": \"3\",\"lastName\": \"Nelson\",\"phone\": \"0987654432\",\"state\": \"AL\",\"zipCode\": \"879652\"}";
+
+
+            IRestResponse response = billingOrder.UpdateOrder("2", body);
+            TestContext.WriteLine(response.Content);
+        }
+
+        [Test]
+        public void TC_DeleteOrder()
+        {
+
+            IRestResponse response = billingOrder.GetOrderById("5");
+            TestContext.WriteLine(response.Content);
+        }
     }
 }
